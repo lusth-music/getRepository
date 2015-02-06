daacb52e343dbe9f20f94c2a7bf7b0053a255e32
jbwolfe <jbwolfe@crimson.ua.edu>
2015-02-06 11:09:24 -0600

    Adding python script

---
+++ getRepository.py
@@ -0,0 +1,35 @@
+import os
+import urllib.request
+from html.parser import HTMLParser
+
+class parseClass(HTMLParser):
+  def handle_starttag(self, tag, attrs):
+    ignore = ["getpack", "instrumentPlayer", "getRepository"]
+    if(tag == "a"):
+      if(attrs[0][0]=='href' and attrs[0][1][1:12]=="lusth-music"):
+        if(attrs[0][1][13:] not in ignore):
+          print()
+          if(not os.path.exists(attrs[0][1][13:])):
+            #print("git clone https://github.com/lusth-music/"+attrs[0][1][13:])
+            os.system("git clone https://github.com/lusth-music/"+attrs[0][1][13:])
+          else:
+            print("Entering: " + attrs[0][1][13:])
+            os.chdir(attrs[0][1][13:])
+            #print("git pull")
+            os.system("git pull")
+            print("Leaving: " + attrs[0][1][13:])
+            os.chdir("..")
+
+def main():
+  url = "https://github.com/lusth-music"
+  try:
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    text = data.decode('utf-8')
+  except:
+    print("Error: could not reach lusth-music repository")
+
+  parser = parseClass()
+  parser.feed(text.strip())
+
+main()
